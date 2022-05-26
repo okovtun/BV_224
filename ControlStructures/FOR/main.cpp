6dec7640e1459e542d8c6b57265b54185b5cf4f8
Oleg <31523835+okovtun@users.noreply.github.com>
2022-05-26 21:48:33 +0300

    FOR loop

--- ControlStructures/FOR/main.cpp
+++ ControlStructures/FOR/main.cpp
@@ -5,5 +5,21 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
+	/*for (;;)
+	{
+		cout << "Hello ";;;;;;;;;;
+	}*/
 
+	int n;
+	cout << "Введите количество итераций: "; 
+	cin >> n;
+//	for(counter; condition; expression)
+	for (
+		int i = 0; 
+		i < n; 
+		i++)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
 }
