2d3ed2ed1357a777c72aba56721bec55b28534e6
Clayman <Clayman@DESKTOP-DM8E77U>
2022-06-07 21:58:29 +0300

    Geometria

--- ControlStructures/Geomtry/main.cpp
+++ ControlStructures/Geomtry/main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 //#define SQUARE
+//#define TRIANGLE_1
+//#define TRIANGLE_2
+//#define TRIANGLE_3
+//#define TRIANGLE_4
+//#define ROMBUS_1
+#define ROMBUS_2
 
 void main()
 {
@@ -20,12 +26,71 @@ void main()
 	}
 #endif // SQUARE
 
+#ifdef TRIANGLE_1
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j <= i; j++)
 		{
 			cout << "* ";
 		}
+		cout << endl;
+	}
+#endif // TRIANGLE_1
+
+#ifdef TRIANGLE_2
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+#endif // TRIANGLE_2
+
+#ifdef TRIANGLE_3
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)cout << "  ";
+		for (int j = i; j < n; j++)cout << "* ";
+		cout << endl;
+	}
+#endif // TRIANGLE_3
+
+#ifdef TRIANGLE_4
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n - 1; j++)cout << "  ";
+		for (int j = 0; j <= i; j++)cout << "* ";
+		cout << endl;
+	}
+#endif // TRIANGLE_4
+
+#ifdef ROMBUS_1
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++)cout << " ";	cout << "/";
+		for (int j = 0; j < i; j++)cout << "  "; cout << "\\";
+		cout << endl;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)cout << " "; cout << "\\";
+		for (int j = i; j < n - 1; j++)cout << "  "; cout << "/";
+		cout << endl;
+	}
+#endif // ROMBUS_1
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++)cout << " "; cout << "/";
+		for (int j = 0; j < i*2; j++)cout << " "; cout << "\\";
+		cout << endl;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)cout << " "; cout << "\\";
+		for (int j = 0; j < (n - i - 1) * 2; j++)cout << " "; cout << "/";
 		cout << endl;
 	}
 }
