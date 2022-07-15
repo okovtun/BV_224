42b1367f3707ca43e3dbdc1ff28f9902cc6585a4
Clayman <Clayman@DESKTOP-DM8E77U>
2022-07-15 20:08:01 +0300

    Array functions in progress

--- Functions/Functions/main.cpp
+++ Functions/Functions/main.cpp
@@ -3,10 +3,17 @@
 #include<iostream>
 using namespace std;
 
-int Add(int a, int b);	//Прототип функции (Объявление функции - Function declaration)
+int Add(int a, int b = 0, int c = 0);	//Прототип функции (Объявление функции - Function declaration)
 int Sub(int a, int b);
 int Mul(int a, int b);
 double Div(int a, int b);
+int Factorial(int n);
+double Power(double a, int n);
+
+//Too few arguments in function call  - если передать в функцию меньше параметров, чем она принимает
+//Too many arguments in function call - если передать в функцию больше параметров, чем она принимает
+//Function doesn't take N arguments
+//default value
 
 void main()
 {
@@ -20,11 +27,11 @@ void main()
 	cout << a << " / " << b << " = " << Div(a, b) << endl;
 }
 
-int Add(int a, int b)//Реализация функции (Определение функции - Function definition)
+int Add(int a, int b, int c)//Реализация функции (Определение функции - Function definition)
 {
 	//Addition - Сложение
-	int c = a + b;
-	return c;
+	int sum = a + b + c;
+	return sum;
 }
 int Sub(int a, int b)
 {
@@ -40,4 +47,18 @@ double Div(int a, int b)
 {
 	//Division - Деление
 	return (double)a / b;
+}
+int Factorial(int n)
+{
+	int f = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		f *= i;
+	}
+	return f;
+}
+double Power(double a, int n)
+{
+	//TODO:
+	return 0;
 }
